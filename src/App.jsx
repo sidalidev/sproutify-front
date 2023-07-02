ba4006928b2453d905a6f8510cbfdcd53f3e4033
Sid Ali Bentifraouine <sidbentifraouine@gmail.com>
2023-07-02 10:34:27 +0200

    mock music

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const MUSICS = [1, 2, 3];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="musics-container">
+      {MUSICS.map((music) => (
+        <div className="music">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e0234e5930e29b7c0f6a315d8fa"
+            alt=""
+          />
+          <p className="title">Your good lies</p>
+          <p className="authors">Vividry</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
